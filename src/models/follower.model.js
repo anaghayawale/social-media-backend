f73e13317a8dbb1e82e50c2e215718cdbb27a85d
anagha <anaghayawale7@gmail.com>
2024-01-16 10:54:55 +0530

    feat: follower controller

--- src/models/follower.model.js
+++ src/models/follower.model.js
@@ -7,25 +7,15 @@ const followerSchema = new Schema(
       ref: "User",
       required: true,
     },
-    followers: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
+    follower: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
   {
     timestamps: true,
   }
 );
-
-followerSchema.pre("save", function (next) {
-  if (this.isNew) {
-    this.followers.forEach((follower, index) => {
-      this.followers[index] = { userId: this._id, follower };
-    });
-  }
-  next();
-});
 
 export const Follower = mongoose.model("Follower", followerSchema);
